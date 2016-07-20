94106e81a0b30e50fc17c412f9e3758e4f538825
Rohit Acharya <rohita77@gmail.com>
2016-07-21 00:12:06 +0800

    Temporaty change to avoid Transpiling of  .json files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -261,7 +261,7 @@ gulp.task('transpile:client', () => {
 });
 
 gulp.task('transpile:server', () => {
-    return gulp.src(_.union(paths.server.scripts, paths.server.json))
+    return gulp.src(_.union(paths.server.scripts))
         .pipe(transpileServer())
         .pipe(gulp.dest(`${paths.dist}/${serverPath}`));
 });
