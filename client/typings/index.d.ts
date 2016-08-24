1ca1f835a188a02b8591168f093762c5e62d76d9
Rohit Acharya <rohita77@gmail.com>
2016-08-24 09:37:17 +0800

    Base Typings

---
+++ client/typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/angular-ui-bootstrap/index.d.ts" />
+/// <reference path="globals/angular-ui-router/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/lodash/index.d.ts" />
+/// <reference path="globals/socket.io-client/index.d.ts" />
