d3501e58162fa93fc7e05bb3db5e82844e791990
Rohit Acharya <rohita77@gmail.com>
2017-07-06 00:26:28 +0800

    Retrieving Watchlist from mongo

--- server/api/watchlist/watchlist.controller.js
+++ server/api/watchlist/watchlist.controller.js
@@ -11,17 +11,19 @@
 
 import _ from 'lodash';
 import Watchlist from './watchlist.model';
+import indices from '../../components/nsedata/indices';
 
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
   return function (entity) {
     if (entity) {
+      //console.log("entity" + entity.name());
       res.status(statusCode).json(entity);
     }
   };
 }
 
-function saveUpdates(updates) {
+export function saveUpdates(updates) {
   return function (entity) {
     var updated = _.merge(entity, updates);
     return updated.save()
@@ -59,59 +61,6 @@ function handleError(res, statusCode) {
   };
 }
 
-function getSymbolsInWatchList(name) {
-  //NSEAdapter.getSymbolsInIndex('Nifty50') --> {symbol,name,id,id, type}
-
-
-  var request = require('request');
-
-  var options = {
-    url: 'https://www.nseindia.com/content/indices/ind_nifty200list.csv',
-    headers: {
-      'Host': 'www.nseindia.com',
-      'Upgrade-Insecure-Requests': 1,
-      'User-Agent': 'Mozilla/5.0(Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36(KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
-      'Accept': 'application/csv,text/csv',
-      'Accept-Encoding': 'gzip, deflate, sdch, br',
-    }
-  };
-
-  //Converter Class
-  var Converter = require("csvtojson").Converter;
-  var converter = new Converter({ constructResult: true }); //for small csv data
-
-  //end_parsed will be emitted once parsing finished
-  var symbolsPromise = new Promise((resolve, reject) => {
-
-    converter.on("end_parsed", function (jsonArray) {
-      jsonArray;
-
-      jsonArray.forEach(elem => {
-        elem.name = elem['Company Name'];
-        delete elem['Company Name'];
-        elem.symbol = elem.Symbol;
-        delete elem.Symbol;
-        elem.industry = elem.Industry;
-        delete elem.Industry;
-        elem.id = elem['ISIN Code'];
-        delete elem['ISIN Code'];
-
-      });
-
-      resolve(jsonArray);    //CB called from another CB?
-      console.log('Found LT:' + jsonArray.find(symbol => { return symbol.symbol.match('\^LT$') }).symbol);
-    });
-
-    request.get(options).pipe(converter);
-
-
-  });
-
-
-  return symbolsPromise;
-}
-
-
 // Gets a list of Watchlists
 export function index(req, res) {
   return Watchlist.find().exec()
@@ -122,26 +71,11 @@ export function index(req, res) {
 // Gets a single Watchlist from the DB
 export function show(req, res) {
 
-  //TD: Remove when symbols are stored in DB
-  if (req.params.id === 'Nifty50') {
-
-    var watchlist = {};
-    watchlist.name = 'Nifty50';
-    watchlist.active = true;
-    watchlist.description = 'Some Index';
-
-    getSymbolsInWatchList('Nifty50')
-      .then(symbols => {
-        watchlist.symbols = symbols;
-        res.status(200).json(watchlist);
-      });
-  }
-  else {
-    return Watchlist.findById(req.params.id).exec()
-      .then(handleEntityNotFound(res))
-      .then(respondWithResult(res))
-      .catch(handleError(res));
-  }
+  console.log("Watchlist Param is" + req.params.id);
+  return Watchlist.findById(req.params.id).exec()
+    .then(handleEntityNotFound(res))
+    .then(respondWithResult(res))
+    .catch(handleError(res));
 }
 
 // Creates a new Watchlist in the DB
--- server/api/watchlist/watchlist.model.js
+++ server/api/watchlist/watchlist.model.js
@@ -3,9 +3,13 @@
 import mongoose from 'mongoose';
 
 var WatchlistSchema = new mongoose.Schema({
-  name: String,
+  _id : String,
+  name: String, //NIFTY 50 Index
   description: String,
   active: Boolean,
+  type: String,   //index
+  subType: String, //NSE.IN
+  downloadKey: String,
   symbols: [ {
         market : String,
         symbol : String,
