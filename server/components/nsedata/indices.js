1cf62b2005fe27f4b9117c1589d182756e9d3f46
Rohit Acharya <rohita77@gmail.com>
2017-07-09 23:48:36 +0800

    refactor to create NSEDataAdapter

--- server/components/nsedata/indices.js
+++ server/components/nsedata/indices.js
@@ -1,50 +1,10 @@
 'use strict';
 
+//import NSEDataAdapter from './index';
+var NSEDataAdapter = require( './index'); //TD:
+
 export function getSymbolsInWatchList(watchlist) {
-  //NSEAdapter.getSymbolsInIndex('Nifty50') --> {symbol,name,id,id, type}
 
-  var request = require('request');
+  return NSEDataAdapter.getSymbolsInIndex(watchlist.downloadKey);
 
-  var options = {
-    url: 'https://www.nseindia.com/content/indices/ind_' + watchlist.downloadKey + '.csv',
-    headers: {
-      'Host': 'www.nseindia.com',
-      'Upgrade-Insecure-Requests': 1,
-      'User-Agent': 'Mozilla/5.0(Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36(KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36',
-      'Accept': 'application/csv,text/csv',
-      'Accept-Encoding': 'gzip, deflate, sdch, br',
-    }
-  };
-
-  //Converter Class
-  var Converter = require("csvtojson").Converter;
-  var converter = new Converter({ constructResult: true }); //for small csv data
-
-  console.log("Downloading Symbols for Watchlist: " + JSON.stringify(watchlist.name) + ' from ' + options.url);
-
-  var symbolsPromise = new Promise((resolve, reject) => {
-
-
-    //end_parsed will be emitted once parsing finished
-    converter.on("end_parsed", function (jsonArray) {
-      resolve(
-        jsonArray.map(elem => {
-          return {
-            name: elem['Company Name']
-            , symbol: elem.Symbol
-            , industry: elem.Industry
-            , _id: elem['ISIN Code']
-          }
-        })
-
-      )
-
-     // console.log('Found LT:' + jsonArray.find(symbol => { return symbol.symbol.match('\^LT$') }).name);
-    });
-
-    request.get(options).pipe(converter);
-
-  });
-
-  return symbolsPromise;
 }
