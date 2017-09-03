f41850f91446dc60267174a75e14fbadf4b34de4
Rohit Acharya <rohita77@gmail.com>
2017-09-03 21:10:16 +0800

    no changes

---
+++ server/components/nse-data-adapter/downloads.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//import NSEDataAdapter from './index';
+var NSEDataAdapter = require( './index'); //TD:
+
+export function getSymbolsInWatchList(watchlist) {
+
+  return NSEDataAdapter.getSymbolsInIndex(watchlist.downloadKey);
+
+}
+
+export function getFCBoardMeetings() {
+
+  return NSEDataAdapter.getBoardMeetings('All_Forthcoming');
+
+}
+
+export function getBoardMeetingsForLast3Months() {
+
+  return NSEDataAdapter.getBoardMeetings('Last_3_Months'); //12 Months? To deal with boundary value past earningg
+
+}
+
+export function getFnOLotSizes() {
+
+  return NSEDataAdapter.getFnOLotSizes(); //No Download Key
+
+}
+
+export function getQuotesForFnOStocks() {
+
+  return NSEDataAdapter.getQuotesForIndexStocks('foSecStockWatch');
+
+}
+
+export function getStockOptionChain(symbol, expiryDate) {
+
+  return NSEDataAdapter.getStockOptionChain(symbol, expiryDate);
+
+}
