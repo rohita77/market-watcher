20ddf249f35892dde75a8736a9f315e448a6ee86
Rohit Acharya <rohita77@gmail.com>
2017-09-03 23:12:13 +0800

    misc

--- server/cheerioTest.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-var optionChainTools = require('./components/nse-data-adapter/index');
-
-optionChainTools.getStockOptionChain2('RELIANCE', '31AUG2017')
-    .then((optionChainData) => console.log(optionChainData))
-
