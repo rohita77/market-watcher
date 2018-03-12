2948b01289e3b5990c45bf94fb7bc27868dae673
Rohit Acharya <rohita77@gmail.com>
2018-03-13 00:47:36 +0800

    Revert "Revert "show recent 35""
    This reverts commit 2bf2664d22ab5b8a488d0a7e7b5f5d2e67e6c093.

--- server/api/daily-stat/daily-stat.controller.js
+++ server/api/daily-stat/daily-stat.controller.js
@@ -66,7 +66,7 @@ function handleError(res, statusCode) {
 
 // Gets a list of DailyStats
 export function index(req, res) {
-  return DailyStat.find().exec()
+  return DailyStat.find().limit(35).sort({"_id" : -1}).exec()
     .then(respondWithResult(res))
     .catch(handleError(res));
 }
