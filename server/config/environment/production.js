c9b79c4037572072a11c24a929c24d4d336f6de9
rohita77 <rohita77@gmail.com>
2020-01-25 15:33:10 +0800

    new mongo db uri

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -16,6 +16,7 @@ module.exports = {
   // MongoDB connection options
   mongo: {
     uri:  process.env.MONGODB_URI ||
+          process.env.MONGOLAB_RED_URI ||
           process.env.MONGOHQ_URL ||
           process.env.OPENSHIFT_MONGODB_DB_URL +
           process.env.OPENSHIFT_APP_NAME ||
